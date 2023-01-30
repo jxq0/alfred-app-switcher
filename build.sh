7a6cc892cf76f51c2c342747145c72e180ce19b6
jxq <jxq@jxq.me>
2023-01-30 16:57:03 +0800

    release

--- build.sh
+++ build.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 set -e
 
-if [ -f "alfred-app-switcher.alfredworkflow" ]; then
+aarch_file=alfred-app-switcher-aarch64.alfredworkflow
+if [ -f $aarch_file ]; then
     echo "  rm old file"
-    rm alfred-app-switcher.alfredworkflow
+    rm $aarch_file
 fi
 
+echo "  build for aarch64-apple-darwin"
 cargo build --release
-
 cp target/release/app-switcher workflow/
 
-cd workflow
+pushd workflow
+zip -r ../$aarch_file *
+popd
 
-zip -r ../alfred-app-switcher.alfredworkflow *
+echo ""
+
+x86_64_file=alfred-app-switcher-x86_64.alfredworkflow
+if [ -f $x86_64_file ]; then
+    echo "  rm old file"
+    rm $x86_64_file
+fi
+
+echo "  build for x86_64-apple-darwin"
+cargo build --release --target x86_64-apple-darwin
+cp target/x86_64-apple-darwin/release/app-switcher workflow/
+
+pushd workflow
+zip -r ../$x86_64_file *
+popd
